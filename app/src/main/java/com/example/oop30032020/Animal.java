4471345ec51e4c23ab9326015ab48cc964bb7d81
phatdroid94@gmail.com <phatdroid94@gmail.com>
2020-04-24 20:46:09 +0700

    tham chieu va tham tri

--- app/src/main/java/com/example/oop30032020/Animal.java
+++ app/src/main/java/com/example/oop30032020/Animal.java
@@ -7,7 +7,17 @@ public class Animal {
     String name;
     int weight;
 
+    // Phương thúc khởi tạo rỗng
+    // Constructor
+    public Animal(String name , int weight){
+        this.name = name;
+        this.weight = weight;
+    }
 
 
     // Phần số 2 : Phương thức (Chức năng của class này hỗ trợ người dùng)
+    public String info(){
+        return "Tên con vật : " + name + "\n" + "Cân nặng : " + weight;
+    }
+
 }
--- app/src/main/java/com/example/oop30032020/MainActivity.java
+++ app/src/main/java/com/example/oop30032020/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.oop30032020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +24,21 @@ public class MainActivity extends AppCompatActivity {
 //                +Quyền lợi khách hàng
 //                +Cộng đồng khách hàng
 
-        Animal meo = new Animal();
+//        Animal meo = new Animal();
+
+//        Log.d("BBB",meo.name);
+//        Button button = new Button(this);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                this.onClick();
+//            }
+//        });
+//        reference
+        // Tham chiếu : liên quan về kiểu dữ liệu object khi truyền thì truyền tham chiếu
+        // Tham trị : liên quan về kiểu dữ liệu nguyên thủy khi truyền thì truyền tham trị
     }
+//    public void showAge(Animal meo){
+//        Toast.makeText(this, age + "", Toast.LENGTH_SHORT).show();
+//    }
 }
